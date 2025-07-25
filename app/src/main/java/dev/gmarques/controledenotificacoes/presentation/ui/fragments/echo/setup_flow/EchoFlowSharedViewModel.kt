7e3f80e3c4e07526e41d7185ac543bedaf96a0d1
Gilian Marques <gilian762@gmail.com>
2025-07-25 14:42:07 -0300

    Added MIT license template to all project files + some fixes

--- app/src/main/java/dev/gmarques/controledenotificacoes/presentation/ui/fragments/echo/setup_flow/EchoFlowSharedViewModel.kt
+++ app/src/main/java/dev/gmarques/controledenotificacoes/presentation/ui/fragments/echo/setup_flow/EchoFlowSharedViewModel.kt
@@ -1,3 +1,28 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Gilian Marques Fernandes - linkedin.com/in/gilianmarques
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
 package dev.gmarques.controledenotificacoes.presentation.ui.fragments.echo.setup_flow
 
 import androidx.lifecycle.ViewModel
@@ -6,6 +31,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.gmarques.controledenotificacoes.data.local.PreferencesImpl
 import dev.gmarques.controledenotificacoes.domain.usecase.installed_apps.GetSmartWatchInstalledAppsUseCase
 import dev.gmarques.controledenotificacoes.presentation.model.InstalledApp
+import dev.gmarques.controledenotificacoes.presentation.ui.fragments.echo.EchoIsEnabledFragment
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -62,7 +88,7 @@ class EchoFlowSharedViewModel @Inject constructor(
  */
 sealed class EchoEvent {
     object EchoOn : EchoEvent()
-    object EchoOff : EchoEvent()
+    object EchoOff : EchoEvent() // TODO: sem uso?
 }
 
 /**
--- app/src/main/java/dev/gmarques/controledenotificacoes/presentation/ui/fragments/splash/SplashViewModel.kt
+++ app/src/main/java/dev/gmarques/controledenotificacoes/presentation/ui/fragments/splash/SplashViewModel.kt
@@ -1,10 +1,33 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2025 Gilian Marques Fernandes - linkedin.com/in/gilianmarques
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
 package dev.gmarques.controledenotificacoes.presentation.ui.fragments.splash
 
-import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dev.gmarques.controledenotificacoes.domain.usecase.user.GetUserUseCase
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -12,6 +35,7 @@ import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import dev.gmarques.controledenotificacoes.presentation.ui.fragments.home.HomeFragment
 
 /**
  * ViewModel responsável por gerenciar o estado do processo de login
@@ -22,11 +46,10 @@ import javax.inject.Inject
  */
 @HiltViewModel
 class SplashViewModel @Inject constructor(
-    @ApplicationContext private val context: Context,
     private val getUserUseCase: GetUserUseCase,
 ) : ViewModel() {
 
-    private val _navigationFlow = MutableStateFlow<NavigationRequirements>(NavigationRequirements())
+    private val _navigationFlow = MutableStateFlow(NavigationRequirements())
     val navigationFlow: StateFlow<NavigationRequirements> get() = _navigationFlow
 
     private val _eventFlow = MutableSharedFlow<SplashEvent>(replay = 1)
